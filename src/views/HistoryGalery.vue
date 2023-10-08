8629e7297644169ce66e4ca3e9ca959704e75dce
scenaristeur <scenaristeur@gmail.com>
2023-10-08 20:01:34 +0200

    rename pics for deploy

--- src/views/HistoryGalery.vue
+++ src/views/HistoryGalery.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <img alt="Vue logo" class="logo" src="@/assets/history/bis4.png" width="125" height="125" />
-        <img alt="Vue logo" class="logo" src="@/assets/history/0_believe_photo ICBINP.png" width="125" height="125" />
-        <img alt="Vue logo" class="logo" src="@/assets/history/1_believe_photo ICBINP.png" width="125" height="125" />
-        <img alt="Vue logo" class="logo" src="@/assets/history/2_believe_photo ICBINP.png" width="125" height="125" />
-        <img alt="Vue logo" class="logo" src="@/assets/history/3_believe_photo ICBINP.png" width="125" height="125" />
-        <img alt="Vue logo" class="logo" src="@/assets/history/4_believe_photo ICBINP.png" width="125" height="125" />
-        <img alt="Vue logo" class="logo" src="@/assets/history/5_believe_photo ICBINP.png" width="125" height="125" />
-        <img alt="Vue logo" class="logo" src="@/assets/history/believe_photo ICBINP.png" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="@/assets/history/0_believe_photo.png" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="@/assets/history/1_believe_photo.png" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="@/assets/history/2_believe_photo.png" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="@/assets/history/3_believe_photo.png" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="@/assets/history/4_believe_photo.png" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="@/assets/history/5_believe_photo.png" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="@/assets/history/believe_photo.png" width="125" height="125" />
         
     </div>
 </template>
