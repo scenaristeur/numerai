6245327e49c25a431644ad0193d8d7d938060ccb
scenaristeur <scenaristeur@gmail.com>
2023-11-18 10:15:13 +0100

    llm trainer

--- src/missions/index.js
+++ src/missions/index.js
@@ -2,5 +2,6 @@ import numerai_mission from "@/missions/numerai_mission.json";
 import quanta_mission from "@/missions/quanta_mission.json";
 import life_genesis_mission from "@/missions/life_genesis_mission.json";
 import brou_enquete_mission from "@/missions/brou_enquete_mission.json";
+import llm_trainer_mission from "@/missions/llm_trainer_mission.json";
 
-export {numerai_mission, quanta_mission, life_genesis_mission, brou_enquete_mission}
+export {numerai_mission, quanta_mission, life_genesis_mission, brou_enquete_mission, llm_trainer_mission}
